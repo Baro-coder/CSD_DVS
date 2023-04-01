5b47119e0ced05f0d862609d68974c90b401d58d
Baro-coder <b.szykula00@gmail.com>
2023-04-01 23:06:41 +0200

    Project structure init
    CMake Project structure initialization

---
+++ app/app.cpp
@@ -0,0 +1,7 @@
+#include "app.hpp"
+
+int main(void)
+{
+    std::cout << "Hello from CSD-DVS Simulator!" << std::endl;
+    return 0;
+}
---
+++ app/app.hpp
@@ -0,0 +1,12 @@
+#ifndef APP_H
+#define APP_H
+
+/* *** Built-in libraries *** */
+#include <iostream>
+
+/* *** Custom libraries *** */
+#include "rand.hpp"
+
+
+
+#endif
